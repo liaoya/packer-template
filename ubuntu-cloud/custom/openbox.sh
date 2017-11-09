675bbb2ed95ee50c125222a2ea4efe53f7a066a8
tom.shen <tom.shen@oracle.com>
2017-11-09 11:48:09 +0800

    Add ubuntu cloud template

--- ubuntu-cloud/custom/openbox.sh
+++ ubuntu-cloud/custom/openbox.sh
@@ -2,5 +2,6 @@
 
 echo "==> Install OpenBox and VNC"
 apt-get install -y -qq -o "Dpkg::Use-Pty=0" xorg openbox xdm xcompmgr cairo-dock vnc4server >/dev/null
-mkdir -p ~/.config/openbox; echo "xterm &" > ~/.config/openbox/autostart; echo "xcompmgr &" >> ~/.config/openbox/autostart; echo "cairo-dock &" >> ~/.config/openbox/autostart
-[[ $SSH_USERNAME ]]su -l $SSH_USERNAME -c 'mkdir -p ~/.config/openbox; echo "xterm &" > ~/.config/openbox/autostart; echo "xcompmgr &" >> ~/.config/openbox/autostart; echo "cairo-dock &" >> ~/.config/openbox/autostart'
+[[ -n $SSH_USERNAME ]] && su -l $SSH_USERNAME -c 'mkdir -p ~/.config/openbox; echo "xterm &" > ~/.config/openbox/autostart; echo "xcompmgr &" >> ~/.config/openbox/autostart; echo "cairo-dock &" >> ~/.config/openbox/autostart'
+[[ -n $SSH_USERNAME ]] && su -l $SSH_USERNAME -c 'mkdir -p $HOME/.vnc; echo -e "123456\n123456\n" | vncpasswd'
+[[ -n $SSH_USERNAME ]] && su -l $SSH_USERNAME -c 'vncserver; sed -i "s/x-window-manager/openbox-session/" ~/.vnc/xstartup; vncserver -kill :1'
