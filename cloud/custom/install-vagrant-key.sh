6b7d0a90c35d4c0d6c2bcedfa0030baf2b8251aa
tom.shen <tom.shen@oracle.com>
2018-03-02 18:32:45 +0800

    Fix vagrant key issue

--- cloud/custom/install-vagrant-key.sh
+++ cloud/custom/install-vagrant-key.sh
@@ -35,8 +35,10 @@ kda/AoGANWrLCz708y7VYgAtW2Uf1DPOIYMdvo6fxIB5i9ZfISgcJ/bbCUkFrhoH
 NE5OgEXk2wVfZczCZpigBKbKZHNYcelXtTt/nP3rsCuGcM4h53s=
 -----END RSA PRIVATE KEY-----
 EOF
-    su -l "$SSH_USER" -c '[ -d ~/.ssh ] ||  { mkdir ~/.ssh; chmod 700 ~/.ssh; }'
+    su -l "$SSH_USER" -c '[ -d ~/.ssh ] || { mkdir ~/.ssh; chmod 700 ~/.ssh; }'
     su -l "$SSH_USER" -c '[ -f /tmp/vagrant.pub ] && cp /tmp/vagrant.pub ~/.ssh/id_rsa.pub && chmod 644 ~/.ssh/id_rsa.pub'
     su -l "$SSH_USER" -c '[ -f /tmp/vagrant ] && cp /tmp/vagrant ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa'
-    cp $SSH_USER/.ssh/id_rsa* $SSH_USER/.ssh/authorized_keys ~/.ssh/
+    [ -d ~/.ssh ] || { mkdir ~/.ssh; chmod 700 ~/.ssh; }
+    [ -f /tmp/vagrant.pub ] && cp /tmp/vagrant.pub ~/.ssh/id_rsa.pub && chmod 644 ~/.ssh/id_rsa.pub
+    [ -f /tmp/vagrant ] && cp /tmp/vagrant ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
 fi
