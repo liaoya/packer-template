c0102135f9cdf5ee46d2be9bcdce29f7121b4594
Tom Shen <tom.shen@oracle.com>
2018-06-15 18:08:45 +0800

    Fix install nvm

--- cloud/custom/fedora/install-common.sh
+++ cloud/custom/fedora/install-common.sh
@@ -2,6 +2,6 @@
 
 echo "==> Install Fedora common packages"
 
-dnf install -y -q zip unzip bzip2 xz fish screen tmux sshpass fish git tig
+dnf install -y -q zip unzip bzip2 xz fish screen tmux sshpass fish git tig nano vim
 
 [[ -n ${CUSTOM_UPDATE} && "${CUSTOM_UPDATE}" == "true" ]] || yum -y -q update
--- cloud/custom/install-nvm.sh
+++ cloud/custom/install-nvm.sh
@@ -5,14 +5,14 @@ echo "==> Install nvm"
 
 NVM_VERSION=$(curl -s "https://api.github.com/repos/creationix/nvm/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 export NVM_DIR=/opt/nvm
-[[ -d ${NVM_DIR} ]] && mkdir -p NVM_DIR
+[[ -d ${NVM_DIR} ]] || mkdir -p NVM_DIR
 
 curl -s -o- https://raw.githubusercontent.com/creationix/nvm/${NVM_VERSION}/install.sh | bash
 
-if [[ -n "$(ls -A ${NVM_DIR})" ]]; then
+if [[ -d ${NVM_DIR} && -n "$(ls -A ${NVM_DIR})" ]]; then
     sed -i "/NVM_DIR/d" ~/.bashrc
     [[ -n ${SSH_USERNAME} ]] && chown -R "$(id -u ${SSH_USERNAME}):$(id -g ${SSH_USERNAME})" ${NVM_DIR}
-    echo "[[ -s $NVM_DIR/nvm.sh ]] && export NVM_DIR=$NVM_DIR && \. \$NVM_DIR/nvm.sh" | tee /etc/profile.d/nvm.sh
+    echo "[[ -s ${NVM_DIR}/nvm.sh ]] && export NVM_DIR=${NVM_DIR} && \. \${NVM_DIR}/nvm.sh" | tee /etc/profile.d/nvm.sh
     [[ -d /etc/bash_completion.d ]] || mkdir -p /etc/bash_completion.d
-    [[ -s ${NVM_DIR}/bash_completion ]] && cp $NVM_DIR/bash_completion /etc/bash_completion/asdf_completion.d
+    [[ -s ${NVM_DIR}/bash_completion ]] && cp ${NVM_DIR}/bash_completion /etc/bash_completion/asdf_completion.d
 fi
