9bc50ce8e742c82b19e345923fd5168c98c0c6f9
Tom Shen <tom.shen@oracle.com>
2019-02-20 13:55:56 +0800

    Install docker-compose as well as install docker

---
+++ cloud/custom/install-docker-compose.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [[ -n ${CUSTOM_DOCKER} && "${CUSTOM_DOCKER}" == "true" ]] || [[ -n ${CUSTOM_DOCKER_CE} && "${CUSTOM_DOCKER_CE}" == "true" ]]; then
+    DOCKER_COMPOSE_VERSION=$(curl -s "https://api.github.com/repos/docker/compose/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+    curl -sL "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    curl -sL https://raw.githubusercontent.com/docker/compose/${DOCKER_COMPOSE_VERSION}/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+fi
