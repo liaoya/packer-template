8726f7de05d0b29195626f85c25a52abe2734351
Tom Shen <tom.shen@oracle.com>
2019-05-05 09:40:22 +0800

    Add fedora 30 and update Ubuntu

--- cloud/custom/ubuntu/install-ppa.sh
+++ cloud/custom/ubuntu/install-ppa.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-add-apt-repository -y ppa:kelleyk/emacs
-add-apt-repository -y ppa:fish-shell/release-3
-add-apt-repository -y ppa:kimura-o/ppa-tig
-add-apt-repository -y ppa:lazygit-team/release
-add-apt-repository -y ppa:alexlarsson/flatpak
+add-apt-repository -u -y ppa:kelleyk/emacs
+add-apt-repository -u -y ppa:fish-shell/release-3
+add-apt-repository -u -y ppa:kimura-o/ppa-tig
+add-apt-repository -u -y ppa:lazygit-team/release
+add-apt-repository -u -y ppa:alexlarsson/flatpak
 # apt-get install -y -qq -o "Dpkg::Use-Pty=0"  flatpak >/dev/null
 # flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
